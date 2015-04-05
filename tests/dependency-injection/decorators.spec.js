a5fdcbf43e873b0f7859e7698c4bd85187f6a6d1
Mike Graham <mgraham@technicalmedia.com>
2015-04-05 03:44:36 -0600

    first decorator test working

--- tests/dependency-injection/decorators.spec.js
+++ tests/dependency-injection/decorators.spec.js
@@ -1,5 +1,46 @@
-describe("a suite", function () {
-    it("true spec", function () {
-        expect(true).toBe(true);
+var __decorate = this.__decorate || function (decorators, target, key, value) {
+    var kind = typeof (arguments.length == 2 ? value = target : value);
+    for (var i = decorators.length - 1; i >= 0; --i) {
+        var decorator = decorators[i];
+        switch (kind) {
+            case "function": value = decorator(value) || value; break;
+            case "number": decorator(target, key, value); break;
+            case "undefined": decorator(target, key); break;
+            case "object": value = decorator(target, key, value) || value; break;
+        }
+    }
+    return value;
+};
+var A = (function () {
+    function A() {
+        this.message = "identity: class A";
+    }
+    return A;
+})();
+var B = (function () {
+    function B() {
+        this.message = "identity: class B";
+    }
+    return B;
+})();
+var D = (function () {
+    function D(a, b) {
+    }
+    D.metadata = [];
+    D = __decorate([decoTest], D);
+    return D;
+})();
+describe("decorators", function () {
+    it("should inject constructor types", function () {
+        var d = new D(new A(), new B());
+        expect(D.metadata.length).toBe(2);
     });
 });
+function decoTest(target) {
+    var rest = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        rest[_i - 1] = arguments[_i];
+    }
+    target["metadata"].push(A, B); // how do i get this based on constructor ???
+    return target;
+}
--- tests/dependency-injection/decorators.spec.ts
+++ tests/dependency-injection/decorators.spec.ts
@@ -1,5 +1,27 @@
-describe("a suite", function() {
-  it("true spec", function() {
-    expect(true).toBe(true);
+
+class A {
+  public message = "identity: class A";
+}
+
+class B {
+  public message = "identity: class B";
+}
+
+@decoTest
+class D {
+  static metadata:Array<Function> = [];
+  constructor(a: A, b: B) {
+  }
+}
+describe("decorators", function() {
+  it("should inject constructor types", function() {
+    var d = new D(new A(), new B());
+    expect(D.metadata.length).toBe(2);
   });
 });
+
+
+function decoTest<T>(target: T, ...rest) {
+  target["metadata"].push(A, B); // how do i get this based on constructor ???
+  return target;
+}
